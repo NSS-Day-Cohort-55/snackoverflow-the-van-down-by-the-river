4965d2ad0f71e0895e2bb216d06398148d30f365
wes-mitchell <wmdrums@gmail.com>
2022-03-25 14:41:21 -0500

    adds veg icon and sorts by price

--- assets/js/menu/MenuManger.js
+++ assets/js/menu/MenuManger.js
@@ -1,7 +1,7 @@
 const apiURL = "http://localhost:8088";
 
 export const getFoods = () => {
-    return fetch(`${apiURL}/foods`)
+    return fetch(`${apiURL}/foods?_sort=price&_order=asc`)
     .then(response => response.json())
 }
 export const getMenus = () => {
